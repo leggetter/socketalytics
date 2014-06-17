e904f9f4d86e51cd56d01d008f6588d08b00b127
leggetter <phil@leggetter.co.uk>
2014-06-17 08:01:10 +0100

    first draft

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function(socket){
+  console.log( 'connection' );
+  socket.on( 'client-data', function( data ) {
+    console.log( data );
+  } );
+  socket.on('mousemove', function(msg){
+    // console.log( msg );
+    io.emit('mousemove', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
