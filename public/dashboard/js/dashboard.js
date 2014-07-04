d0c9ffa8b3a5f81f42257818d104964d60d6610f
leggetter <phil@leggetter.co.uk>
2014-07-04 21:14:14 +0100

    Removing solution files for getting-started

--- public/dashboard/js/dashboard.js
+++ public/dashboard/js/dashboard.js
@@ -1,29 +1,3 @@
 $( function() {
-  localStorage.debug = 'socket.io-client:socket';
-
-  var visitors = $('#visitors').epoch( {
-    type: 'time.area', axes: ['left', 'bottom', 'right'],
-    data: [ { values: [ { time: Date.now()/1000, y: 0 } ] } ]
-  } );
-  var pages = $( '#pages' ).epoch( { type: 'bar' } );
-  var touch = $( '#touch' ).epoch( { type: 'time.gauge' } );
-  var video = $( '#video' ).epoch( { type: 'time.gauge' } );
-
-  var dashboard = io( 'localhost:3000/dashboard' );
-  dashboard.on( 'stats-updated', function( update ) {
-
-    // Convert to percentages
-    touch.update( ( update.touch / update.connections ) || 0 );
-    video.update( ( update.video / update.connections ) || 0 );
-
-    var pagesData = [];
-    for( var url in update.pages ) {
-      pagesData.push( { x: url, y: update.pages[ url ] } );
-    }
-    pages.update( [ { values: pagesData } ] );
-
-    visitors.push( [ { time: Date.now()/1000, y: update.connections } ] );
-
-  } );
-
+  // Add code here
 } );
